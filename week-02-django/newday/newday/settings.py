b1dfe263e9a4d2d10f5af94e9297ef88b6c1521d
unknown <minhae7942@gmail.com>
2019-03-11 23:18:53 +0900

    week2 is finished in 4months

--- week-02-django/newday/newday/settings.py
+++ week-02-django/newday/newday/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'polls'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'newday.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, "temp")],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,7 +117,11 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
+# Static files (CSS, JavaScript, Images)
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+]
--- week-02-django/newday/polls/views.py
+++ week-02-django/newday/polls/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
-
+from .models import Article
+from django.http import HttpResponse
 # Create your views here.
+
+def index(request):
+    # return HttpResponse("Hello! it's yesterday!")
+    # name = "minnie"
+    article_list = Article.objects.all()
+    # Article.objects.create(
+    #     title="hello",
+    #     contents="this is test",
+    #     view_count=0
+    # )
+    ctx={
+        "article_list":article_list
+    }
+    return render(request, "index.html", ctx )
+    # return HttpResponse(article.title)
